b951d95498950d89f18411d27c729e455da3f249
BruceMustache <viniciusxnicolas007@gmail.com>
2019-08-15 20:20:40 -0300

    order function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # TODO modularizar projeto
+# TODO messagem ORDER_LIST vazio
+# TODO congratulations
+# TODO separar por capitulos
+# TODO perguntar se ordena aleatorio
+# TODO profiles
+# TODO more efficient show_oder
 
 from random import shuffle
 from database import DATABASE
@@ -29,12 +35,17 @@ def show_order():
     global DONE
     global GREEN, RED, RESET_COLORS
 
-    for index, question in enumerate(ORDER_LIST):
-        question_done = DONE[index]
-        color = GREEN if question_done else RED
-        print(f'{color}{question}\t--\tdone: {question_done}{RESET_COLORS}')
+    for index in range(len(ORDER_LIST)):
+       show_single_question(index)
 
 
+def show_single_question(index):
+    question = ORDER_LIST[index]
+    question_done = DONE[index]
+    color = GREEN if question_done else RED
+    print(f'{color}{question}\t--\tdone: {question_done}{RESET_COLORS}')
+
+   
 def manager():
     show_order()
     done_question = int(input('What question you want to add as done? '))
@@ -55,8 +66,21 @@ def help_message():
     print('type "set" to set another order')
     print('type "show" to show last order')
     print('type "manager" to manage your questions')
+    print('type "order" to do questions in order')
     print('type "help" to show this messages')
     print('type "exit" to exit')
+
+
+def done_in_order():
+    for index in range(len(ORDER_LIST)):
+        if not DONE[index]:
+            show_single_question(index)
+            answer = input(':: ')
+            if answer == 'done':
+                DONE[index] = True
+            elif answer == 'break':
+                break
+    update_database()
  
 
 if __name__ == '__main__':
@@ -71,6 +95,8 @@ if __name__ == '__main__':
             manager()
         elif answer == 'help':
             help_message()
+        elif answer == 'order':
+            done_in_order()
         elif answer == 'exit':
             exit()
 
