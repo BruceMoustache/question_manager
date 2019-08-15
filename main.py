f2c7b0ff421862e4f89b7df34de6a731a90b6656
BruceMustache <viniciusxnicolas007@gmail.com>
2019-08-15 19:41:32 -0300

    changing messages: adding new functionalitys: exit, help(developing)

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def new_order():
     ORDER_LIST.extend(list(range(first_question_number, last_qustion_number+1)))
     DONE.extend([False] * len(ORDER_LIST))
     shuffle(ORDER_LIST)
-    print(DATABASE)
-    print(ORDER_LIST)
     update_database()
 
 
@@ -32,7 +30,7 @@ def show_order():
     for index, question in enumerate(ORDER_LIST):
         question_done = DONE[index]
         color = GREEN if question_done else RED
-        print(f'{color}{question} -- done: {question_done}')
+        print(f'{color}{question} -- done: {question_done}{RESET_COLORS}')
 
 
 def manager():
@@ -41,7 +39,6 @@ def manager():
     done_question_index = ORDER_LIST.index(done_question)
     DONE[done_question_index] = True
     update_database()
-    print(f'{DONE[done_question_index]} -- {done_question_index}{RESET_COLORS}')
 
 
 def update_database():
@@ -53,18 +50,19 @@ def update_database():
 
 
 if __name__ == '__main__':
-    print('type "set" if you want to set another order')
-    print('type "show" if you want to show last order')
-    print('type "manager" if you want to manage your questions')
+    print('type "set" to set another order')
+    print('type "show" to show last order')
+    print('type "manager" to manage your questions')
+    print('type "help" to show this messages')
+    print('type "exit" to exit')
     while True:
-        try:
-            answer = input('--> ')
-            if answer == 'set':
-                new_order()
-            elif answer == 'show':
-                show_order()
-            elif answer == 'manager':
-                manager()
-        except KeyboardInterrupt:
-            break
+        answer = input('--> ')
+        if answer == 'set':
+            new_order()
+        elif answer == 'show':
+            show_order()
+        elif answer == 'manager':
+            manager()
+        elif answer == 'exit':
+            exit()
 
